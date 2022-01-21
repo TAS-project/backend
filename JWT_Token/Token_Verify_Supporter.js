2705765c4014b91874724001e5be4622c9304ae9
AnitaKamani <aniakamani@gmail.com>
2022-01-21 15:27:10 +0330

    minor touch ups

--- JWT_Token/Token_Verify_Supporter.js
+++ JWT_Token/Token_Verify_Supporter.js
@@ -1,23 +1,19 @@
 const Token = require("./JWT_Token");
-const Response = require("../Responses/general");
 
-const verifyToken = async (req, res, next) => {
-	let response = new Response();
-
-	if (!req.headers.authorization) {
-		response.setStatus(401).setMessage("fail").setRes("fail");
-		return res.status(401).send(response.handler());
+exports.verifyToken = async (req, res, next) => {
+	try {
+		if (!req.headers.authorization) {
+			res.status(401).send("Please login again");
+		}
+		const result = await Token.verifyToken(req, 0);
+		if (result[0]) {
+			//it was Ok and token had no error
+			res.status(200);
+			next();
+		} else {
+			res.status(401).send("Please login again");
+		}
+	} catch (e) {
+		res.status(401).send("Please login again");
 	}
-	const result = await Token.verifyToken(req, 0);
-	if (result[0]) {
-		//it was Ok and token had no error
-	} else {
-		response.setStatus(401).setMessage("fail").setRes("fail");
-		return res.status(401).send(response.handler());
-	}
-	next();
-};
-
-module.exports = {
-	verifyToken,
 };
--- JWT_Token/Token_Verify_User.js
+++ JWT_Token/Token_Verify_User.js
@@ -3,7 +3,7 @@ const Token = require("./JWT_Token");
 exports.verifyToken = async (req, res, next) => {
 	try {
 		if (!req.headers.authorization) {
-			res.status(401).send("Error");
+			res.status(401).send("Please login again");
 		}
 		const result = await Token.verifyToken(req, 1);
 		if (result[0]) {
@@ -11,9 +11,9 @@ exports.verifyToken = async (req, res, next) => {
 			res.status(200);
 			next();
 		} else {
-			res.status(401).send("Error");
+			res.status(401).send("Please login again");
 		}
 	} catch (e) {
-		res.status(401).send("Error");
+		res.status(401).send("Please login again");
 	}
 };
