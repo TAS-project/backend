d76e0bc5f0100a84a773ffc11c21146078ec65be
AnitaKamani <aniakamani@gmail.com>
2022-01-23 22:56:43 +0330

    tmp

--- Controller/Supporter/Genre_Create.js
+++ Controller/Supporter/Genre_Create.js
@@ -4,9 +4,9 @@ const Genre = require("../../models/Genre");
 exports.create = async (req, res, next) => {
 	try {
 		let new_genre = new Genre({
-			Name: req.body.Name,
+			Name: req.body.Genre_Title,
 			Color: req.body.Color,
-			About: req.body.About,
+			About: req.body.Summary,
 		});
 		await new_genre.save();
 		res.status(200).send({ Response: "Done" });
