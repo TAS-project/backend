623205154300b1901b015d1754a7e57b9626bb65
AnitaKamani <aniakamani@gmail.com>
2022-01-24 00:43:18 +0330

    Book Profile modified

--- Controller/Both/Book_Profile.js
+++ Controller/Both/Book_Profile.js
@@ -1,19 +1,46 @@
 const Book = require("../../models/Book");
-const Date_Tool = require("../../Tools/Date_Tool");
+const User = require("../../models/User");
+const Genre = require("../../models/Genre");
+const Book_Genre = require("../../models/Book_Genre");
 
 // retrieving book details
 exports.view = async (req, res, next) => {
 	try {
-		const load_Book = await Book.findOne({
+		const load_book = await Book.findOne({
 			where: { ID: req.body.Book_ID },
 		});
-		if (load_Book == null) {
+		if (load_book == null) {
 			res.status(400).send({ Response: "Error not such a book!" });
 		} else {
-			const Last_Updated = Date_Tool.make_readable(load_book.updatedAt);
-			// const Last_Updated = Date_Tool.make_readable(load_book.updatedAt);
+			const writer = await User.findOne({
+				where: { ID: load_book.UserID },
+			});
+			const genre_ids = await Book_Genre.findAll({
+				where: { BookID: load_book.ID },
+			});
 
-			res.status(200).send({ Response: "Done", Book: load_Book });
+			var genres = [];
+			for (var i = 0, l = genre_ids.length; i < l; i++) {
+				const genre = await Genre.findOne({
+					where: { ID: genre_ids[i].GenreID },
+				});
+				genres.push({ name: genre.Name, color: genre.Color });
+			}
+
+			const book_found = {
+				User_ID: req.person.ID,
+				Book_ID: load_book.ID,
+				Book_Name: load_book.Name,
+				Writer_ID: writer.ID,
+				Writer_Username: writer.Username,
+				writer: writer.First_Name + " " + writer.Last_Name,
+				BooK_Rate: load_book.Rating,
+				genres: genres,
+				followed: 666,
+				photo: null,
+			};
+
+			res.status(200).send({ Response: "Done", Book: book_found });
 		}
 	} catch (e) {
 		res.status(400).send({ Response: "Error" });
