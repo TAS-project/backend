a05b1e095c10156338a9b412e002e860932df690
AnitaKamani <aniakamani@gmail.com>
2022-01-25 02:47:36 +0330

    bug fixed

--- Controller/Both/Chapter.js
+++ Controller/Both/Chapter.js
@@ -2,6 +2,7 @@ const Chapter = require("../../models/Chapter");
 const User = require("../../models/User");
 const Book = require("../../models/Book");
 const Date_Tool = require("../../Tools/Date_Tool");
+const Comment = require("../../models/Comment");
 
 // create new Chapter
 exports.create = async (req, res, next) => {
@@ -32,6 +33,26 @@ exports.view = async (req, res, next) => {
 			where: { ID: book_of_chapter.UserID },
 		});
 		const Publishing_date = Date_Tool.make_readable(wanted_chapter.createdAt);
+
+		var Comments = [];
+		const load_Comments = await Comment.findAll({
+			where: { ChapterID: req.body.Chapter_ID },
+			order: [["ID", "DESC"]],
+		});
+		for (var i = 0, l = load_Comments.length; i < l; i++) {
+			const Commenter = await User.findOne({
+				where: { ID: load_Comments[i].UserID },
+			});
+			const comment_found = {
+				Comment_ID: load_Comments[i].ID,
+				Comment: load_Comments[i].Content,
+				Commenter_ID: Commenter.ID,
+				Commenter_Username: Commenter.Username,
+				pic: Commenter.Pic,
+			};
+			Comments.push(comment_found);
+		}
+
 		const founded_chapter = {
 			Chapter_ID: wanted_chapter.ID,
 			chapter_name: wanted_chapter.Name,
@@ -40,6 +61,7 @@ exports.view = async (req, res, next) => {
 			content: wanted_chapter.Content,
 			Owner_ID: owner_of_book.ID,
 			Owner_Username: owner_of_book.Username,
+			Comments: Comments,
 		};
 		res.status(200).send({ Response: "Done", Chapter: founded_chapter });
 	} catch (e) {
