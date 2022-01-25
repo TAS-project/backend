6d70376217f7644aebe393861367aa913afdb3be
AnitaKamani <aniakamani@gmail.com>
2022-01-25 22:12:37 +0330

    back done

--- Controller/User/Home.js
+++ Controller/User/Home.js
@@ -2,14 +2,28 @@ const Book = require("../../models/Book");
 const User = require("../../models/User");
 const Chapter = require("../../models/Chapter");
 const Date_Tool = require("../../Tools/Date_Tool");
+const Bookmark = require("../../models/Bookmark");
 
 // retrieving Home details
 exports.Home_get = async (req, res, next) => {
 	try {
 		var Books = [];
-		let load_book = await Book.findAll({
+		var load_book = [];
+		let tmp = await Book.findAll({
 			where: { UserID: req.person.ID },
 		});
+		load_book = load_book.concat(tmp);
+
+		let tmp2 = await Bookmark.findAll({
+			where: { UserID: req.person.ID },
+		});
+
+		for (var i = 0, l = tmp2.length; i < l; i++) {
+			let tmp3 = await Book.findOne({
+				where: { ID: tmp2[i].BookID },
+			});
+			load_book.push(tmp3);
+		}
 		for (var i = 0, l = load_book.length; i < l; i++) {
 			const writer = await User.findOne({
 				where: { ID: load_book[i].UserID },
